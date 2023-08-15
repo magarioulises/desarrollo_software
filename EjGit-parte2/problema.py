004d5af480b91b7b9bfd35ea4c785f7be54edcf4
magariou <ulisesmagario33@gmail.com>
2023-08-15 19:22:38 -0300

    Cambio

---
+++ EjGit-parte2/problema.py
@@ -0,0 +1,16 @@
+"""Escriba un algoritmo que permita al usuario ingresar 2 valores numéricos
+ y seguidamente muestre un menú como el siguiente:
+
+1. Ingresar valor 1
+2. Ingresar valor 2
+3. Mostrar suma
+4. Mostrar resta
+5. Mostrar multiplicación
+6. Mostrar división
+7. Salir
+
+Elija una opción:
+-> El programa debe continuar pidiendo al usuario que seleccione una 
+opción hasta que se ingrese 7 (Salir).
+-> Si se selecciona alguna de las opciones: 3, 4, 5 o 6 sin antes haber pasado 
+por las opciones 1 y 2 (ingresar valores) se debe mostrar un mensaje de error."""
